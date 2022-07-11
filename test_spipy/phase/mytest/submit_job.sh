654cb2e0f70c2952354aca6717c43ef2618438d3
XIAN Yin, Poline <poline3939@gmail.com>
2022-07-11 13:22:57 +0800

    Delete test_spipy directory

--- test_spipy/phase/mytest/submit_job.sh
+++
@@ -1,2 +0,0 @@
-# Submit the command below to your job submitting system to run 3d phasing
-/Users/yin/anaconda2/bin/mpirun -n 2 /Users/yin/anaconda2/bin/python /Users/yin/anaconda2/lib/python2.7/site-packages/spipy/phase/template_3d/phase.py /Users/yin/Downloads/spipy-examples/test_spipy/phase/mytest/input.h5
